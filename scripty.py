c8b1c712456695e057adf23d547f0e8ca6176114
Omar Jabour <jabour.omar@hotmail.com>
2025-11-16 16:34:34 +0100

    init new class for Propertites v.1

--- scripty.py
+++ scripty.py
@@ -79,5 +79,9 @@ class Car:
     def __init__(self, brand, model, year):
         self.brand = brand
         self.model = model
+        self.year = year
+
+my_car = Car("Tesla", "Model 3", 2024)
+
         
 

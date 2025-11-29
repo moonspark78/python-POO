4e4e862c1248549b9475b1c4501f02fba3bc8824
Omar Jabour <jabour.omar@hotmail.com>
2025-11-29 21:00:05 +0100

    :art: poo Absraction v.0 create the EmailService class

--- scripty.py
+++ scripty.py
@@ -214,4 +214,19 @@ test_account.withdraw(1)
 
 
 """ ------------- Absraction ------------- """
+class EmailService:
+    
+    def _connect(self):
+        print("Connecting to email server...")
+    
+    def _authenticate(self):
+        print("Authenticating...")
+        
+    def send_email(self):
+        self._connect()
+        self._authenticate()
+        print("Sending email...")
+        
+    def _disconnect(self):
+        print("Disconnecting from email server...")
 
